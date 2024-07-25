4f65c3b27321e441458dd4afd3fde4898b354158
itavr <itayavrechstudio@gmail.com>
2024-07-26 00:49:36 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+let font;
+let typing = "";
+let indent;
+let n;
+let letters = [];
+
+function preload() {
+  // טוען את הגופן
+  font = loadFont('ploni-light-aaa.otf');
+}
+
+function setup() {
+  createCanvas(480, 480);
+  indent = 200;
+  n = 16;
+  textSize(18);
+  textFont(font);
+}
+
+function draw() {
+  background(0); // רקע שחור
+  fill(255); // טקסט לבן
+  textAlign(LEFT);
+
+  text(typing, indent, height / 2);
+
+  for (let l of letters) {
+    l.update();
+    l.display();
+  }
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    let x = indent;
+    for (let i = 0; i < typing.length; i++) {
+      letters.push(new Letter(x, height / 2, typing.charAt(i)));
+      x += textWidth(typing.charAt(i));
+    }
+    typing = "";
+  } else if (keyCode === BACKSPACE) {
+    if (typing.length > 0) {
+      typing = typing.substring(0, typing.length - 1);
+    }
+  } else if (key.length === 1 && typing.length <= n) {
+    // בדיקה אם התו הוא תו תקני להדפסה
+    typing += key;
+  }
+}
+
+class Letter {
+  constructor(x, y, letter) {
+    this.letter = letter;
+    this.location = createVector(x, y);
+    this.velocity = createVector(random(-1, 1), random(-1, 1)); // מהירות אקראית לכל כיוון
+    this.gravity = createVector(0, 0.001); // גרביטציה קלה מאוד
+  }
+
+  display() {
+    fill(255); // טקסט לבן
+    text(this.letter, this.location.x, this.location.y);
+  }
+
+  update() {
+    this.location.add(this.velocity);
+    this.velocity.add(this.gravity);
+
+    // שינוי כיוון אם האותיות יוצאות מחוץ לקנבס
+    if (this.location.x > width || this.location.x < 0) {
+      this.velocity.x *= -1;
+    }
+    if (this.location.y > height || this.location.y < 0) {
+      this.velocity.y *= -1;
+    }
+  }
+}
